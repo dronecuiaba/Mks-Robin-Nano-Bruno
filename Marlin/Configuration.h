f4cf857ea68b897c40fbc6684cc483b79c961ba3
dronecuiaba <dronecuiaba@gmail.com>
2022-04-09 13:11:31 -0400

    cc

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1179,7 +1179,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -30, -2, -0.15 }
+#define NOZZLE_TO_PROBE_OFFSET { -30, -2, -0.12 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1378,7 +1378,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 200
+#define Z_MAX_POS 240
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
